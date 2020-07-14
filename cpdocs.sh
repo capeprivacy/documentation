489d2d85e15d11b38932c1ad64757129bf8fe4d6
Deborah Barnard <starfallprojects@gmail.com>
2020-07-14 09:51:59 +0100

    updated script

--- cpdocs.sh
+++ cpdocs.sh
@@ -7,10 +7,10 @@ git submodule update --remote
 # Generate the Cape Python docs
 echo "Generating Cape Python docs . . . "
 pydoc-markdown
-# pydoc-markdown on Windows generates mkdocs.yml with '\', which Netlify cannot handle: 
+# Keeping this in case we need it again, although Niklas has kindly fixed it: 
 # https://github.com/NiklasRosenstein/pydoc-markdown/issues/129
 # using '#' as a delimeter to make this monstrosity less monstrous
-sed -i -e 's#\\#\/#g' ./pythonv1/mkdocs.yml
+# sed -i -e 's#\\#\/#g' ./pythonv1/mkdocs.yml
 
 # Generate the Cape Core docs
 
